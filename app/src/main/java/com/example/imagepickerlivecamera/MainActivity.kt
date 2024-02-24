00b64ce6a61bf58762926136bcb6d4b0ea567f52
Raquel <raquel.silva@contractor-mc1.com>
2024-02-23 22:33:54 -0300

    Adjusts for manifest and main

--- app/src/main/java/com/example/imagepickerlivecamera/MainActivity.kt
+++ app/src/main/java/com/example/imagepickerlivecamera/MainActivity.kt
@@ -1,15 +1,16 @@
-package com.example.imagepickerlivecamera
-
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import com.example.imagepickerlivecamera.R
 import com.example.imagepickerlivecamera.ui.theme.ImagePickerLiveCameraTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,9 +18,8 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             ImagePickerLiveCameraTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(), color = MaterialTheme.colorScheme.background) {
-                    Greeting("Android")
+                    ImageViewer()
                 }
             }
         }
@@ -27,17 +27,19 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-            text = "Hello $name!",
-            modifier = modifier
+fun ImageViewer(modifier: Modifier = Modifier) {
+    Image(
+        painter = painterResource(id = R.drawable.ic_majoras_test_background),
+        contentDescription = "@null",
+        modifier = modifier.fillMaxSize(),
+        contentScale = ContentScale.Crop
     )
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun ImageViewerPreview() {
     ImagePickerLiveCameraTheme {
-        Greeting("Android")
+        ImageViewer()
     }
 }
